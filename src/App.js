a38df709d89528411e63da078d839783b8c94498
Manuel Camejo <manuelcamejo76@hotmail.com>
2022-08-01 20:44:20 -0300

    bootstrap and navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navbar>
+        <Container>
+          <Navbar.Brand href="#home">
+            <span className='algo'>Algo</span><span className='tron'>Tron</span>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
   );
 }
 
