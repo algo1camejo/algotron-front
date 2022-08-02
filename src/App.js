cdeb5ddab11474def52367a31238b0933b8d500b
mellamopepino <delfinagarciav@gmail.com>
2022-08-01 21:22:09 -0300

    [DES] Agregar custom theming
    - Se agrega sass y bootstrap.
    - Se genera custom theme.
    - Se borran estilos no usados.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom.scss';
 import './App.css';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
-      <Navbar>
+      <Navbar bg="dark">
         <Container>
           <Navbar.Brand href="#home">
             <span className='algo'>Algo</span><span className='tron'>Tron</span>
