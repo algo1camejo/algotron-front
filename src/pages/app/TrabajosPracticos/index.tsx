8e199e89ee85ef4f5db2463a911a94b3ebd46c17
mellamopepino <delfinagarciav@gmail.com>
2022-09-14 15:22:00 -0300

    [FIX] No siempre se muestra el 404 de tron (#17)
    Problema:
    - En algunos paths inválidos no se muestra el 404 de tron.
      Esto pasaba por tener paths nesteados sin 404 interno.
      Ej: `trabajos-practicos/url/invalido`.
    
    Solución:
    - Se sacan las routes nesteadas y se las deja a todas en el mismo lugar,
      de esta forma todas agarran la misma página de 404 como default.

--- src/pages/app/TrabajosPracticos/index.tsx
+++ src/pages/app/TrabajosPracticos/index.tsx
@@ -1,16 +1,2 @@
-import { FC } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Entrega from './Entrega';
-import TrabajosPracticos from './TrabajosPracticos';
-import './styles.scss';
-
-export const TrabajosPracticosRoutes: FC = () => {
-  return (
-    <Routes>
-      <Route path=":id" element={<Entrega/>}/>
-      <Route path="/" element={<TrabajosPracticos/>}/>
-    </Routes>
-  );
-};
-
-export default TrabajosPracticosRoutes;
+export { TrabajosPracticos } from './TrabajosPracticos';
+export { Entrega } from './Entrega';
