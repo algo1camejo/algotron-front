8faa503d8e4317c18282886ae4d45bbaf142ec3b
Nicolas <nriedel@fi.uba.ar>
2022-11-09 17:37:16 -0300

    query to documents

--- src/services/material.ts
+++ src/services/material.ts
@@ -2,5 +2,5 @@ import api from "src/config/api";
 import { MaterialResponse } from "src/types/Material";
 
 export const getMaterial = () => (
-  api.get<MaterialResponse>(`material/api/v1/material/`)
+  api.get<MaterialResponse>(`material/api/v1/material/?curso=1&anio=2022&periodo=1`)
 );
