e5ac10b184e1df03f4a9b9f0ff07fbe51e94bbd4
Manuel Iñaki Bilbao <mbilbao@fi.uba.ar>
2023-08-27 19:58:39 -0300

    Cambio de cuatrimestre en material

--- src/services/material.ts
+++ src/services/material.ts
@@ -2,5 +2,5 @@ import api from "src/config/api";
 import { MaterialResponse } from "src/types/Material";
 
 export const getMaterial = () => (
-  api.get<MaterialResponse>(`material/api/v1/material/?curso=1&anio=2023&periodo=1&limit=100`)
+  api.get<MaterialResponse>(`material/api/v1/material/?curso=1&anio=2023&periodo=2&limit=100`)
 );
